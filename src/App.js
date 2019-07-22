6bf0ca54d105e74a530dfbdd8d645fc6f47ce8e4
Jose Montero Jr <josemariodev@gmail.com>
2019-07-22 14:25:50 -0500

    CartContext created and wrapped Provider in App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
   const [products] = useState(data);
@@ -19,10 +20,12 @@ function App() {
   return (
     <div className='App'>
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path='/' component={products} />} />
-        <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={cart}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path='/' component={products} />} />
+          <Route path='/cart' render={() => <ShoppingCart cart={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
